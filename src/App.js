bc7b8de70d42e8213c1c881b0114c51ceb7c4569
Henrique Velardo <Henrique>
2023-07-11 20:18:58 -0300

    implementando div estilizada incial com emotion

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import { Card } from "./componentes/Card/Card";
 import { Estilos } from "./componentes/EstilosGlobais/Estilos";
 
 function App() {
   return (
     <div>
       <Estilos />
-     <h1>Freelando</h1>
+      <Card>
+        <h1>Freelando</h1>
+      </Card>
     </div>
   );
 }
